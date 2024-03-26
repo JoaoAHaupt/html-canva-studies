db0ba70ef2d41f9d8c88c0f6eece4d1b359b8025
JoaoAHaupt <joao.haupt@aluno.ifsp.edu.br>
2024-03-26 10:57:59 -0300

    bolinhas

--- canvas02-animating/canvas.js
+++ canvas02-animating/canvas.js
@@ -2,33 +2,71 @@ var canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 var c = canvas.getContext('2d');
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dy = (Math.random() - 0.5) * 30;
-var dx = (Math.random() - 0.5) * 30;
-var radius = 30;
+c.globalCompositeOperation = 'destination-over'
+canvas.style.backgroundColor = "black";
+
+function Circle(x, y, dx, dy, radius){
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    var color = 'white';
+
+    this.draw = function(){
+        c.beginPath();
+        
+        c.arc(this.x, this.y, 30, 0, Math.PI * 2, false);
+        c.strokeStyle = color
+        c.stroke();
+        c.fillStyle = color;
+        c.fill()
+    }
+    this. update = function(x,y){
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0){
+            this.dx = -this.dx;
+            var randomColor = Math.floor(Math.random()*16777215).toString(16);
+            color = '#' + randomColor
+        }
+        if (this.y  > innerHeight || this.y - this.radius < 0){
+            this.dy = -this.dy;
+            var randomColor = Math.floor(Math.random()*16777215).toString(16);
+            color = '#' + randomColor
+        }
+       
+        this.x += this.dx;
+        this.y += this.dy;
+        this.draw();
+
+    }
+}
+var circleArray = [];
+
+for(var i = 0; i < 100; i++){
+
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dy = (Math.random() - 0.5) * 6;
+    var dx = (Math.random() - 0.5) * 6;
+    var radius = 30;
+
+    var circle = new Circle(x, y, dx, dy, radius);
+    circleArray.push( circle)
+
+}
+    
+console.log(circleArray)
+
 animate();
 
 function animate(){
 
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
-
-    c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.stroke();
-    if (x + radius > innerWidth || x - radius < 0){
-        dx = -dx;
+    for(var i = 0; i < circleArray.length; i ++){
+        circleArray[i].update();
     }
-    if (y + radius > innerHeight || y - radius < 0){
-        dy = -dy;
-    }
-   
-    x += dx;
-    y += dy;
-
 }
 
 
